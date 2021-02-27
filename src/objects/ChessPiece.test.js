6730c0b18a7b77f82ce0a2c11878c062994e0b57
JGMEYER <meyer.justian@gmail.com>
2021-02-27 15:12:42 -0500

    More robust fromFEN() validation

--- src/objects/ChessPiece.test.js
+++ src/objects/ChessPiece.test.js
@@ -8,7 +8,7 @@ import ChessBoardState from '../objects/ChessBoardState';
 describe('ChessPiece.js', () => {
     describe('_validMovesAlongLine', () => {
         test('handles check correctly', () => {
-            const chessBoardState = ChessBoardState.fromFEN('8/8/8/8/3n4/8/1BK5/8 w - - 0 1')
+            const chessBoardState = ChessBoardState.fromFEN('k7/8/8/8/3n4/8/1BK5/8 w - - 0 1')
             const bishop = chessBoardState.getFileRank('b2');
             const validBishopMoves = bishop.validMoves(chessBoardState);
             expect(validBishopMoves).toEqual([new Move([6, 1], [4, 3])]);
